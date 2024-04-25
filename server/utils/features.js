6e7f12eeebe25b65780278753b90e98205cfea17
AniruddhUdayan <aniruddhudayan34@gmail.com>
2024-04-26 00:42:23 +0530

    cookies changes

--- server/utils/features.js
+++ server/utils/features.js
@@ -7,7 +7,7 @@ import { getBase64, getSockets } from "../lib/helper.js";
 const cookieOptions = {
   maxAge: 15 * 24 * 60 * 60 * 1000,
   sameSite: 'None',
-  httpOnly: true,
+  httpOnly: false,
   secure: true,
 };
 
